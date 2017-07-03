8a7fb13dfb07833c52c1ff5a6beb2869f07815e1
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-07-03 15:12:40 -0600

    Reset AddFishForm after fish is submitted

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -11,11 +11,12 @@ class AddFishForm extends React.Component {
       image: this.image.value
     }
     this.props.addFish(fish)
+    this.fishForm.reset()
   }
 
   render() {
     return (
-      <form className="fish-edit" onSubmit={event => this.createFish(event)}>
+      <form ref={(input => this.fishForm = input)} className="fish-edit" onSubmit={event => this.createFish(event)}>
         <input ref={(input) => this.name = input} type="text" placeholder="Fish Name"/>
         <input ref={(input) => this.price = input} type="text" placeholder="Fish Price"/>
         <select ref={(input) => this.status = input}>
