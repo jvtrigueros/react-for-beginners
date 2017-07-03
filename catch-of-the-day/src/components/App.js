8ea6d1cabe361b7081f92aef5b42695036c5c349
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-07-03 15:49:50 -0600

    Increment order count based on the fish key

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     super()
     this.addFish = this.addFish.bind(this)
     this.loadSamples = this.loadSamples.bind(this)
+    this.addToOrder = this.addToOrder.bind(this)
     this.state = {
       fishes: {},
       order: {}
@@ -33,6 +34,12 @@ class App extends React.Component {
     this.setState({fishes})
   }
 
+  addToOrder(key) {
+    const order = {...this.state.order}
+    order[key] = order[key] + 1 || 1
+    this.setState({order})
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -42,7 +49,7 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key => <Fish key={key} idx={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
