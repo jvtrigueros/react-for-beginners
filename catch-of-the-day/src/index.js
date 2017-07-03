e17adc99fcbee3fb190b355e4a1db59ea7d9454a
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-07-03 12:10:45 -0600

    Add Header, Inventory, and Order; convert Header to stateless fn

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -5,4 +5,4 @@ import './css/style.css'
 import StorePicker from './components/StorePicker'
 import App from './components/App'
 
-render(<StorePicker/>, document.querySelector('#main'))
+render(<App/>, document.querySelector('#main'))
