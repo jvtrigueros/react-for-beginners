cd41dd3f0d435409e18a695f40966b6e06b7bc68
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-07-03 13:33:32 -0600

    Prepare StorePicker to begin routing to a specific store url

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super()
+  //   this.goToStore = this.goToStore.bind(this)
+  // }
+
+  goToStore(event) {
+    event.preventDefault()
+    // grab text from box
+    console.log(this.storeInput.value)
+    // transition from / -> /store/:storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={event => this.goToStore(event)}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
         <button type="submit">Visit Store -></button>
       </form>
     )
